fa3e0007a2bc59f5d8a5b3ed709701ddef8db9bb
Mark Prins <prinsmc@minlnv.nl>
2013-10-22 14:06:47 +0200

    cleanup YSlow integration tests
    en gerelateerde jsp validatie tests

--- src/test/java/nl/mineleni/cbsviewer/yslow/YSlowIntegrationTest.java
+++ src/test/java/nl/mineleni/cbsviewer/yslow/YSlowIntegrationTest.java
@@ -8,12 +8,9 @@ package nl.mineleni.cbsviewer.yslow;
 
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.IsNot.not;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.io.RandomAccessFile;
 
 import nl.mineleni.cbsviewer.IntegrationTestConstants;
 
@@ -21,8 +18,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * YSlow Testcase voor index.jsp.
@@ -30,12 +25,8 @@ import org.slf4j.LoggerFactory;
  * @author mprins
  */
 public class YSlowIntegrationTest extends IntegrationTestConstants {
-	/** logger. */
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(YSlowIntegrationTest.class);
 	private Process phantomProcess;
 	private ProcessBuilder pb;
-	private int allowedToFail = 0;
 
 	/**
 	 * aanmaken van de rapportage directory, dat blijkt phantomjs + yslow niet
@@ -49,36 +40,29 @@ public class YSlowIntegrationTest extends IntegrationTestConstants {
 	}
 
 	/**
-	 * Voorbereiding van {@linkplain http://yslow.org/phantomjs/ phantomjs} 
-	 * proces voor de testcases.
+	 * voorbereidingen van phantomjs proces voor de testcases.
 	 * 
 	 * @throws Exception
 	 */
 	@Before
 	public void preparePhantomJS() throws Exception {
-		this.pb = new ProcessBuilder("phantomjs");
+		pb = new ProcessBuilder("phantomjs");
+		pb.command().add("target/yslow.js");
+		pb.command().add("--verbose");
 
-		this.pb.command().add("target/yslow.js");
-		this.pb.command().add("-v");
-		
-		this.pb.command().add("-r");
-		this.pb.command().add("yblog");
+		pb.command().add("--format");
+		pb.command().add("junit");
 
-		this.pb.command().add("-f");
-		this.pb.command().add("junit");
+		pb.command().add("--info");
+		pb.command().add("grade");
 
-		this.pb.command().add("-i");
-		this.pb.command().add("grade");
+		pb.command().add("--threshold");
+		pb.command().add("B");
 
-		this.pb.command().add("-t");
-		this.pb.command().add(
-				"{overall:'B', 'ycdn':'F', 'yexpires':'F', 'ycompress':'F'}");
-		allowedToFail = /* 3x 'F' */3 + /* 1 ignored */1;
+		pb.command().add("--viewport");
+		pb.command().add("800x600");
 
-		this.pb.command().add("-vp");
-		this.pb.command().add("800x600");
-
-		this.pb.redirectError(new File("phantomjs_error.log"));
+		pb.redirectError(new File("phantomjs_error.log"));
 	}
 
 	/**
@@ -86,8 +70,6 @@ public class YSlowIntegrationTest extends IntegrationTestConstants {
 	 */
 	@After
 	public void stopPhantomJS() {
-		LOGGER.debug("PhantomJS process stopped, exit code: "
-				+ this.phantomProcess.exitValue());
 		this.phantomProcess.destroy();
 	}
 
@@ -98,24 +80,13 @@ public class YSlowIntegrationTest extends IntegrationTestConstants {
 	 */
 	@Test
 	public void phantomYSlowIndexRIATest() throws Exception {
-		this.pb.command().add(BASE_TEST_URL + "index.jsp");
-		this.pb.redirectOutput(new File("target/yslow/TEST-YSlowIndexRIA.xml"));
-
-		LOGGER.debug("PhantomJS commandline options: " + this.pb.command());
-		this.phantomProcess = this.pb.start();
+		pb.command().add(BASE_TEST_URL + "index.jsp");
+		pb.redirectOutput(new File("target/yslow/TEST-YSlowIndexRIA.xml"));
+		this.phantomProcess = pb.start();
 
 		assertThat("Number of YSlow violations returned.",
-				this.phantomProcess.waitFor(), not(greaterThan(allowedToFail)));
+				this.phantomProcess.waitFor(), not(greaterThan(0)));
 
-		try (RandomAccessFile report = new RandomAccessFile(
-				"target/yslow/TEST-YSlowIndexRIA.xml", "r")) {
-			assertFalse("Running test has failed.", report.readLine()
-					.startsWith("FAIL to load undefined"));
-
-			report.seek(0);
-			assertTrue("Running test has failed.", report.readLine()
-					.startsWith("<?xml version="));
-		}
 	}
 
 	/**
@@ -125,22 +96,11 @@ public class YSlowIntegrationTest extends IntegrationTestConstants {
 	 */
 	@Test
 	public void phantomYSlowIndexCORETest() throws Exception {
-		this.pb.command().add(BASE_TEST_URL + "index.jsp?coreonly=true");
-		this.pb.redirectOutput(new File("target/yslow/TEST-YSlowIndexCORE.xml"));
-		LOGGER.debug("PhantomJS commandline options: " + this.pb.command());
-		this.phantomProcess = this.pb.start();
+		pb.command().add(BASE_TEST_URL + "index.jsp?coreonly=true");
+		pb.redirectOutput(new File("target/yslow/TEST-YSlowIndexCORE.xml"));
+		this.phantomProcess = pb.start();
 
 		assertThat("Number of YSlow violations returned.",
-				this.phantomProcess.waitFor(), not(greaterThan(allowedToFail)));
-
-		try (RandomAccessFile report = new RandomAccessFile(
-				"target/yslow/TEST-YSlowIndexRIA.xml", "r")) {
-
-			assertFalse("Running test has failed.", report.readLine()
-					.startsWith("FAIL to load undefined"));
-			report.seek(0);
-			assertTrue("Running test has failed.", report.readLine()
-					.startsWith("<?xml version="));
-		}
+				this.phantomProcess.waitFor(), not(greaterThan(0)));
 	}
 }
